29654eef401f5438cf297a73c0132d15f1c492f1
bananta <ba26mail-goods@yahoo.com>
2011-05-20 22:10:10 +1000

    fix unit test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,16 +7,15 @@ describe UsersController do
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
+  describe "GET 'show'" do
+
+    before(:each) do
+      @user = Factory(:user)
     end
 
-    it "should have the right title" do
-      get 'new'
-      response.should have_selector("title",
-                                    :content => @base_title +  " | Sign Up")
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
     end
 
     it "should find the right user" do
@@ -37,6 +36,19 @@ describe UsersController do
     it "should have a profile image" do
       get :show, :id => @user
       response.should have_selector("h1>img", :class => "gravatar")
+    end
+  end
+
+  describe "GET 'new'" do
+    it "should be successful" do
+      get 'new'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'new'
+      response.should have_selector("title",
+                                    :content => @base_title +  " | Sign Up")
     end
   end
 
